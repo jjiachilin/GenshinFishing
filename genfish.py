5823fce482f6e5cd6584a3c82d0a1907df9a328c
lukededau <lukededau@gmail.com>
2021-09-12 16:39:59 -0700

    optimized clicking speed

--- genfish.py
+++ genfish.py
@@ -1,13 +1,22 @@
+from PIL.ImageOps import grayscale
 import pyautogui
+from PIL import Image
+import time
 
 def main():
+    count = 0
+    pointer_image = Image.open('images/pointer.png')
     while True:
-        
-        if pyautogui.locateOnScreen('images/box.png', confidence=0.8):
-            print('box found')
+        ptr_region = pyautogui.locateOnScreen(pointer_image, confidence=0.75, grayscale=True, region=(800, 80, 800, 100))
+        box_region = pyautogui.locateOnScreen(pointer_image, confidence=0.75, grayscale=True, region=(800, 80, 800, 100))
+        print(ptr_region)
+        if ptr_region:
+            print(str(count) + ': pointer found')
             pyautogui.click()
         else:
-            print('no box')
+            print(str(count) + ': no pointer')
+        
+        count += 1
 
 if __name__ == "__main__":
     main()
