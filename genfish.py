37b3354ed5fc92bf953846e6775ac71b0b4376a5
lukededau <lukededau@gmail.com>
2021-09-12 17:47:50 -0700

    implemented click logic

--- genfish.py
+++ genfish.py
@@ -1,18 +1,34 @@
 from PIL.ImageOps import grayscale
 import pyautogui
 from PIL import Image
-import time
 
 def main():
     count = 0
     pointer_image = Image.open('images/pointer.png')
+    left_bound_image = Image.open('images/box_left.png')
+    right_bound_image = Image.open('images/box_right.png')
+    pyautogui.PAUSE = 0.01
+
     while True:
-        ptr_region = pyautogui.locateOnScreen(pointer_image, confidence=0.75, grayscale=True, region=(800, 80, 800, 100))
-        box_region = pyautogui.locateOnScreen(pointer_image, confidence=0.75, grayscale=True, region=(800, 80, 800, 100))
-        print(ptr_region)
+        ptr_region = pyautogui.locateOnScreen(pointer_image, confidence=0.75, grayscale=True, region=(800, 140, 800, 40))
+        left_bound = pyautogui.locateOnScreen(left_bound_image, confidence=0.6, grayscale=True, region=(800, 140, 800, 40))
+        right_bound = pyautogui.locateOnScreen(right_bound_image, confidence=0.6, grayscale=True, region=(800, 140, 800, 40))
+        print("ptr: " + str(ptr_region))
+        # print("left: " + str(left_bound))
+        # print("right: " + str(right_bound))
         if ptr_region:
             print(str(count) + ': pointer found')
-            pyautogui.click()
+            box_left_val = box_right_val = -1
+            if left_bound:
+                box_left_val = left_bound.left + left_bound.width / 2
+            if right_bound:
+                box_right_val = right_bound.left + right_bound.width / 2
+            # box_middle_val = (box_left_val + box_right_val) / 2
+            ptr_middle_val = ptr_region.left + ptr_region.width / 2
+            if ptr_middle_val < box_right_val:
+                pyautogui.click()
+            elif ptr_middle_val < box_left_val:
+                pyautogui.click(duration=1)
         else:
             print(str(count) + ': no pointer')
         
